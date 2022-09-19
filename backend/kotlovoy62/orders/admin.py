0ed6ae29c74add7874014a2f8f12880329581210
Hash466 <g.h466h@gmail.com>
2022-09-19 22:18:09 +0300

    создание модели order

--- backend/kotlovoy62/orders/admin.py
+++ backend/kotlovoy62/orders/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Order, OrderItem
+
+EMPTY_VALUE = '-пусто-'
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('element',)
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'number', 'last_name', 'first_name', 'status', 'comment',
+        'created'
+    )
+    empty_value_display = EMPTY_VALUE
+    list_editable = ('comment', 'status')
+    search_fields = ('number', '')
+    list_filter = ('status', 'created', 'updated',)
+    inlines = [OrderItemInline]
+
+
+admin.site.register(Order, OrderAdmin)
--- backend/kotlovoy62/orders/models.py
+++ backend/kotlovoy62/orders/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+from users.models import User
+from catalog.models import Element
+
 ORDER_STATUS = (
         ('order_is_completed', 'заказ выполнен'),
         ('order_in_progress', 'заказ на этапе выполнения'),
@@ -9,6 +12,18 @@ ORDER_STATUS = (
         ('new_order', 'новый заказ'),
         ('order_cancelled', 'заказ отменён'),
     )
+
+DELIVERY = (
+    ('SDEK', 'СДЭК'),
+    ('Yandex', 'Яндекс'),
+    ('Russian_post', 'Почта России'),
+)
+
+PAYMENT = (
+    ('by_fact', 'при получении'),
+    ('online', 'онлайн'),
+    ('cashless', 'безналичный расчет'),
+)
 
 
 class Order(models.Model):
@@ -24,6 +39,14 @@ class Order(models.Model):
     status = models.CharField(
         max_length=50, choices=ORDER_STATUS, default='new_order',
         verbose_name='Статус заказа',
+    )
+    delivery = models.CharField(
+        max_length=50, choices=DELIVERY, default='SDEK',
+        verbose_name='Доставка',
+    )
+    payment = models.CharField(
+        max_length=50, choices=PAYMENT, default='by_fact',
+        verbose_name='Оплата',
     )
     comment = models.CharField(
         max_length=250, blank=True, null=True, verbose_name='Комментарий',
@@ -57,3 +80,48 @@ class Order(models.Model):
         max_length=200, blank=True, null=True,
         verbose_name='Улица, дом, квартира',
     )
+    user = models.ForeignKey(
+        User, verbose_name='Пользователь',
+        on_delete=models.RESTRICT, related_name='orders',
+        blank=True, null=True,
+    )
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='items',
+        verbose_name='Заказ',
+    )
+    element = models.ForeignKey(
+        Element,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='order_items',
+        verbose_name='Деталь',
+    )
+    price = models.PositiveIntegerField(
+        verbose_name='Цена',
+    )
+    cur_price = models.PositiveIntegerField(
+        verbose_name='Цена со скидкой',
+    )
+    quantity = models.PositiveIntegerField(
+        default=1,
+        verbose_name='Количество',
+    )
+
+    class Meta:
+        verbose_name = 'Элемент'
+        verbose_name_plural = 'Элементы'
+
+    def __str__(self):
+        return '{}'.format(self.element)
+
+    def get_cost(self):
+        return self.cur_price * self.quantity
